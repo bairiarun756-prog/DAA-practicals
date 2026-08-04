2854fb9947b2c6766b0774ef1794bda6cfce04d0
Bairi ArunKumar <bairiarun756@gmail.com>
2026-08-04 06:53:39 +0530

    Add new file

--- DAA-practical-3/heepsort.cpp
+++ DAA-practical-3/heepsort.cpp
@@ -1,60 +1,71 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> 
+#include <algorithm>
 
-void heapify(std::vector<int>& arr, int n, int i) {
-    int largest = i;         
-    int leftChild = 2 * i + 1;  
-    int rightChild = 2 * i + 2; 
-    
+using namespace std;
+
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int leftChild = 2 * i + 1;
+    int rightChild = 2 * i + 2;
+
     if (leftChild < n && arr[leftChild] > arr[largest]) {
         largest = leftChild;
     }
 
-    
     if (rightChild < n && arr[rightChild] > arr[largest]) {
         largest = rightChild;
     }
 
-    
     if (largest != i) {
-        std::swap(arr[i], arr[largest]);
+        swap(arr[i], arr[largest]);
 
         heapify(arr, n, largest);
     }
 }
 
-void heapSort(std::vector<int>& arr) {
+void heapSort(vector<int>& arr) {
     int n = arr.size();
 
-
+    // Build max heap
     for (int i = n / 2 - 1; i >= 0; i--) {
         heapify(arr, n, i);
     }
 
     for (int i = n - 1; i > 0; i--) {
-        std::swap(arr[0], arr[i]);
+        swap(arr[0], arr[i]);
 
         heapify(arr, i, 0);
     }
 }
 
-void printArray(const std::vector<int>& arr) {
+
+void printArray(const vector<int>& arr) {
     for (int num : arr) {
-        std::cout << num << " ";
+        cout << num << " ";
     }
-    std::cout << "\n";
+    cout << endl;
 }
 
 int main() {
-    std::vector<int> data = {12, 11, 13, 5, 6, 7};
-    
-    std::cout << "Original array: ";
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> data(n);
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
+    }
+
+    cout << "\nOriginal array: ";
     printArray(data);
 
     heapSort(data);
 
-    std::cout << "Sorted array:   ";
+    cout << "Sorted array: ";
     printArray(data);
 
     return 0;
